9a93fb95e551dd4c3646949dfd4aa67f08312d80
neerajkumarsi20 <neerajtnj201@gmail.com>
2025-02-28 10:44:15 +0530

    first commit

---
+++ Simon Game Challenge Starting Files/script.js
@@ -0,0 +1,100 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#current-score").textContent = level
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  updateScore()
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  $("#current-score").text("Current Score: 0");
+  gamePattern = [];
+  started = false;
+}
+
+var highScore = localStorage.getItem("highScore") || 0; // Retrieve high score
+$("#high-score").text("High Score: " + highScore); // Display high score
+
+function updateScore() {
+    var current_score = level - 1;
+    $("#current-score").text("Current Score: " + current_score);
+
+    if ((level - 1) > highScore) {
+
+        highScore = level - 1;
+        localStorage.setItem("highScore", highScore); // Update high score in localStorage
+        $("#high-score").text("High Score: " + highScore);
+
+        $("#high-score-message").text("🎉 New High Score! 🎉").fadeIn().delay(2000).fadeOut();
+    }
+}
+
+
